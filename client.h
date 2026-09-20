532ba44456c117a072a8eb9b2e034c3d3663f641
hadNAU <lhh37@nau.edu>
2026-09-20 15:12:10 -0700

    changed the address and port back to government server

--- client.h
+++ client.h
@@ -2,13 +2,13 @@
 #define DAYTIME_CLIENT_H
 
 // Server information for the NIST Daytime service
-//#define SERVER_ADDRESS   "time-a.nist.gov"
-//#define SERVER_PORT      "13"
+#define SERVER_ADDRESS   "time-a.nist.gov"
+#define SERVER_PORT      "13"
 
 
 //LOCAL MACHINE TESTING INFORMATION
-#define SERVER_ADDRESS "127.0.0.1"
-#define SERVER_PORT "23657"
+//#define SERVER_ADDRESS "127.0.0.1"
+//#define SERVER_PORT "23657"
 
 // MAX_MESSAGE_LENGTH is 80
 #define MAX_MESSAGE_LENGTH 80
